576535989aad1dc13857b32928ad7e199da32690
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2024-04-30 22:02:11 +0200

    zadanie3
    dodanie zadania3

---
+++ 1/zadanie3/zadanie3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ścieżka do pliku tekstowego
+        string sciezkaDoPliku = "D:\\studia\\programowanie .net\\dotnet\\1\\zadanie3\\zadanie3/text.txt";
+
+        try
+        {
+            // Otwarcie pliku za pomocą FileStream
+            using (FileStream fs = new FileStream(sciezkaDoPliku, FileMode.Open, FileAccess.Read))
+            {
+                // Utworzenie czytnika StreamReader
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    // Odczytanie i wyświetlenie zawartości pliku linia po linii
+                    string linia;
+                    while ((linia = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linia);
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Plik nie istnieje.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Błąd wejścia-wyjścia: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wystąpił błąd: " + ex.Message);
+        }
+    }
+}
